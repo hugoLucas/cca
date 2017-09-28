f005f2247945561a270e9b0a9553f0171d6e9317
hugoLucas <hugojlucas@gmail.com>
2017-09-27 19:22:31 -0700

    Activity for presenting user Banknote created.

---
+++ app/src/main/java/com/example/hugolucas/cca/ClassificationActivity.java
@@ -0,0 +1,13 @@
+package com.example.hugolucas.cca;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClassificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classification);
+    }
+}
