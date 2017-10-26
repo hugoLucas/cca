bbf8152a32a7e5f9ff02c94a5ce20e62cff61f67
hugoLucas <hugojlucas@gmail.com>
2017-10-25 18:30:36 -0700

    ResultsActivity created, basic layout added

---
+++ app/src/main/java/com/example/hugolucas/cca/ResultsActivity.java
@@ -0,0 +1,11 @@
+package com.example.hugolucas.cca;
+
+/**
+ * Presents the results of the classification to the User. Allows the user to access MapFragment
+ * and ExchangeFragment.
+ *
+ * Created by hugo on 10/25/17.
+ */
+
+public class ResultsActivity {
+}
