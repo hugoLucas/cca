a7b6dc75801ba991a8d8e9bf7f2aa0b6d6202503
hugolucas <hugojlucas@gmail.com>
2017-10-27 10:00:28 -0700

    Mapbox works now

---
+++ app/src/main/java/com/example/hugolucas/cca/DebugActivity.java
@@ -0,0 +1,16 @@
+package com.example.hugolucas.cca;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by hugolucas on 10/27/17.
+ */
+
+public class DebugActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment(Bundle savedInstanceState) {
+        return new MapFragment();
+    }
+}
--- app/src/main/java/com/example/hugolucas/cca/MapFragment.java
+++ app/src/main/java/com/example/hugolucas/cca/MapFragment.java
@@ -1,11 +1,39 @@
 package com.example.hugolucas.cca;
 
-import android.support.v13.app.FragmentCompat;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+
+import static com.example.hugolucas.cca.R.id.mapView;
 
 /**
  * Created by hugolucas on 10/26/17.
  */
 
-public class MapFragment extends FragmentCompat {
+public class MapFragment extends Fragment{
 
+    private String apiKey = "pk.eyJ1IjoiaHVnb2Y3NTYiLCJhIjoiY2o5OTc4N3I0MHBjZTJ3cW1tYXBsOTUzZyJ9.h2WlhaCYJMlkF_Nq88NDVg";
+    private MapView mapView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Mapbox.getInstance(getActivity(), apiKey);
+        mapView = getView().findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+    }
 }
