27160e16da2bcf5619f50501d530992c1f63e3f4
hugolucas <hugojlucas@gmail.com>
2017-10-26 22:19:20 -0700

    Added single fragment activity to make debugging easier

---
+++ app/src/main/java/com/example/hugolucas/cca/MapFragment.java
@@ -0,0 +1,11 @@
+package com.example.hugolucas.cca;
+
+import android.support.v13.app.FragmentCompat;
+
+/**
+ * Created by hugolucas on 10/26/17.
+ */
+
+public class MapFragment extends FragmentCompat {
+
+}
