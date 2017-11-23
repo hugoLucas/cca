6de912141f4cccbb0ec573a57bf730cabf9235b1
hugolucas <hugojlucas@gmail.com>
2017-11-22 19:34:32 -0800

    Constants for SharedPreferences added, #6

---
+++ app/src/main/java/com/example/hugolucas/cca/constants/Settings.java
@@ -0,0 +1,17 @@
+package com.example.hugolucas.cca.constants;
+
+/**
+ * Class holds keys needed to access user settings from a SharedPreferences object.
+ *
+ * Created by hugolucas on 11/22/17.
+ */
+
+public class Settings {
+
+    public static final String DB = "settings.db";
+    public static final String IMG = "settings.image";
+    public static final String CUR = "settings.currency";
+    public static final String RAD = "settings.radius";
+    public static final String INT = "settings.interval";
+
+}
