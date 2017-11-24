543c66262b7e1e41c288ceaa379d317f18c3f80a
hugolucas <hugojlucas@gmail.com>
2017-11-23 19:48:12 -0800

    Created DBGalleryActivity and layout file, #12

---
+++ app/src/main/java/com/example/hugolucas/cca/DBGalleryActivity.java
@@ -0,0 +1,12 @@
+package com.example.hugolucas.cca;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hugolucas on 11/23/17.
+ */
+
+public class DBGalleryActivity extends AppCompatActivity {
+
+
+}
